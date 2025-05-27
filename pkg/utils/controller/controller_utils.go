07baaea4d1b88c6b7ce595b9b10126c340e517ab
Peter Pan <Peter.Pan@daocloud.io>
2025-05-27 13:43:35 +0800

    implement for KEP services
    Signed-off-by: Peter Pan <Peter.Pan@daocloud.io>

--- pkg/utils/controller/controller_utils.go
+++ pkg/utils/controller/controller_utils.go
@@ -30,33 +30,43 @@ import (
 )
 
 func CreateHeadlessServiceIfNotExists(ctx context.Context, k8sClient client.Client, Scheme *runtime.Scheme, lws *leaderworkerset.LeaderWorkerSet, serviceName string, serviceSelector map[string]string, owner metav1.Object) error {
+	return CreateServiceIfNotExists(ctx, k8sClient, Scheme, lws, serviceName, serviceSelector, true, owner)
+}
+
+func CreateServiceIfNotExists(ctx context.Context, k8sClient client.Client, Scheme *runtime.Scheme, lws *leaderworkerset.LeaderWorkerSet, serviceName string, serviceSelector map[string]string, isHeadlessService bool, owner metav1.Object) error {
 	log := ctrl.LoggerFrom(ctx)
-	// If the headless service does not exist in the namespace, create it.
-	var headlessService corev1.Service
-	if err := k8sClient.Get(ctx, types.NamespacedName{Name: serviceName, Namespace: lws.Namespace}, &headlessService); err != nil {
+	// If the service does not exist in the namespace, create it.
+	var service corev1.Service
+	if err := k8sClient.Get(ctx, types.NamespacedName{Name: serviceName, Namespace: lws.Namespace}, &service); err != nil {
 		if client.IgnoreNotFound(err) != nil {
 			return err
 		}
-		headlessService := corev1.Service{
+		service := corev1.Service{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      serviceName,
 				Namespace: lws.Namespace,
 				Labels:    map[string]string{leaderworkerset.SetNameLabelKey: lws.Name},
 			},
 			Spec: corev1.ServiceSpec{
-				ClusterIP:                "None", // defines service as headless
 				Selector:                 serviceSelector,
 				PublishNotReadyAddresses: true,
 			},
 		}
+		if isHeadlessService {
+			service.Spec.ClusterIP = "None" // defines service as headless
+		}
 
 		// Set the controller owner reference for garbage collection and reconciliation.
-		if err := ctrl.SetControllerReference(owner, &headlessService, Scheme); err != nil {
+		if err := ctrl.SetControllerReference(owner, &service, Scheme); err != nil {
 			return err
 		}
 		// create the service in the cluster
-		log.V(2).Info("Creating headless service.")
-		if err := k8sClient.Create(ctx, &headlessService); err != nil {
+		if isHeadlessService {
+			log.V(2).Info("Creating headless service.")
+		} else {
+			log.V(2).Info("Creating service.")
+		}
+		if err := k8sClient.Create(ctx, &service); err != nil {
 			return err
 		}
 	}
